11e26d62d279f2cb3cb71ff3b710ea2bea7350fc
Hermes Mesquita <hermesmesquita@gmail.com>
2025-11-25 23:11:50 -0300

    aprendendo sobre fontes

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,9 @@ import { inter } from './ui/fonts';
 export default function RootLayout({children,}: {  children: React.ReactNode;}) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
